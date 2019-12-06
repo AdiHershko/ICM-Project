11c77df2d3a76b98882bb34efc76efaf05efa7b6
adi <adihershkovitzz@gmail.com>
2019-12-06 21:05:59 +0200

    added auto reconnect

--- AvTipus/src/client/ChatClient.java
+++ AvTipus/src/client/ChatClient.java
@@ -4,6 +4,7 @@
 
 package client;
 
+import java.awt.JobAttributes.DialogType;
 import java.io.IOException;
 
 import application.Controller;
@@ -13,16 +14,66 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Dialog;
 import ocsf.client.AbstractClient;
 
 public class ChatClient extends AbstractClient {
 
 	final public static int DEFAULT_PORT = 5555;
+	Dialog<Boolean> noConnection = new Dialog();
 
 	public ChatClient(String host, int port) throws IOException {
 		super(host, port); // Call the superclass constructor
 		openConnection();
 		System.out.println("Client connected");
+		//-----------Checking if server is connected every 5 seconds.
+		new Thread(){
+			public void run()
+			{
+				while (true)
+				{
+					if (!isConnected())
+					{
+						Platform.runLater(new Runnable(){
+							public void run()
+							{
+								{
+									if (!noConnection.isShowing())
+									{
+										noConnection.setTitle("ERROR!");
+										noConnection.setContentText("Server disconnected\nTrying to reconnect...");
+										noConnection.show();
+									}
+								}
+							}
+						});
+						try {
+							while (!openConnection()) {
+								try {Thread.sleep(3000);  }catch(InterruptedException e1) {}
+							}
+						} catch (IOException e) {  }
+
+						Platform.runLater(new Runnable(){
+							public void run()
+							{
+							Alert connected = new Alert(AlertType.INFORMATION);
+							connected.setTitle("Reconnected!");
+							connected.setContentText("Reconnected to server!");
+							noConnection.setResult(Boolean.TRUE);
+							noConnection.close();
+							connected.show();
+							}
+						});
+
+					}
+					try{
+						Thread.sleep(5000);
+					} catch(InterruptedException e) {}
+
+				}
+			}
+		}.start();
+   //-------------------------------------------------
 	}
 
 	public void handleMessageFromServer(Object msg) {
@@ -34,7 +85,7 @@ public class ChatClient extends AbstractClient {
 		if (msg instanceof Object[]) {
 			/*
 			 * Platform.runLater(new Runnable() { TODO
-			 * 
+			 *
 			 * @Override public void run() { ObservableList<Request> l =
 			 * FXCollections.observableArrayList(); for (Object o : (Object[]) msg) {
 			 * l.add((Request) o); } Controller._ins.table.setItems(l); } });
@@ -66,6 +117,6 @@ public class ChatClient extends AbstractClient {
 		} catch (IOException e) {
 		}
 		System.exit(0);
-		
+
 	}
 }
