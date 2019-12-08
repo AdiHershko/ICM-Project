32b5d1f1a70c2d1393a37193ba403dbcedc0e331
Zeev <Zeev@DESKTOP-CHFAITK>
2019-12-08 12:10:18 +0200

    Changed hersko's messeage

--- AvTipus/src/client/ChatClient.java
+++ AvTipus/src/client/ChatClient.java
@@ -11,15 +11,17 @@ import application.Request;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogEvent;
 
 public class ChatClient extends AbstractClient {
 
 	final public static int DEFAULT_PORT = 5555;
 	Dialog<Boolean> noConnection = new Dialog<>();
-
+	
 	public ChatClient(String host, int port) throws IOException {
 		super(host, port); // Call the superclass constructor
 		openConnection();
@@ -34,7 +36,7 @@ public class ChatClient extends AbstractClient {
 								{
 									if (!noConnection.isShowing()) {
 										noConnection.setTitle("ERROR!");
-										noConnection.setContentText("Server disconnected\nTrying to reconnect...");
+										noConnection.setContentText("Server disconnected\nPlease re-open and connect the server");
 										noConnection.show();
 									}
 								}
